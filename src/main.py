538aaf3dbc556da611548e76640610c90b6f6a9a
BTELNYY <74313030+BTELNYY@users.noreply.github.com>
2022-08-07 14:36:23 -0600

    new options

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # imports, like using or import
 import shutil
-from os import listdir
+from os import listdir, remove
 from os.path import isfile, join, exists
 version = "v1.0.0"
 print("btelnyy's mc player .dat offline to online converter " + version)
@@ -28,6 +28,18 @@ def getUUID(name) -> str:
     print("UUID: " + getCorrectedUUID(data["id"]))
     return data["id"]
 
+
+def getName(UUID) -> str:
+    print("Getting Name. UUID: " + UUID)
+    data = requests.get("https://sessionserver.mojang.com/session/minecraft/profile/" + UUID.replace("-",""))
+    if(data.status_code != 200):
+        print("Error: An error occured while contacting mojang servers: " + str(data.status_code))
+        # cracked accounts cannot be converted, so they will stay cracked.
+        return UUID
+    data = data.json()
+    print("Name: " + data["name"])
+    return data["name"]
+
 # get formatted UUID, now adds dashes
 def getCorrectedUUID(id) -> str:
     chars = []
@@ -45,26 +57,46 @@ def getCorrectedUUID(id) -> str:
 
 # main function, check files, split and copy
 def main():
-    # For every file in files
-    for file in getFiles():
-        # split name and extension
-        parts = file.split(".")
-        # if the extension is not .dat
-        if(parts[1] != 'dat'):
-            print(file + " is not a .dat file, ignoring.")
-            continue
-        # otherwise
-        else:
-            # get corrected filename
-            filename = getCorrectedUUID(getUUID(parts[0]))
-            # copy files
-            if exists(filename + ".dat"):
-                print("File " + filename + ".dat" + " already exists, keeping old version.")
+    print("Enter an option!")
+    print("1. Convert all name.dat to uuid.dat files")
+    print("2. Print all players who have played into a text file")
+    userinput = input("> ")
+    if userinput == "1":
+        # For every file in files
+        for file in getFiles():
+            # split name and extension
+            parts = file.split(".")
+            # if the extension is not .dat
+            if(parts[1] != 'dat'):
+                print(file + " is not a .dat file, ignoring.")
                 continue
-            # debug logging
-            print("Copying file: " + file + " -> " + filename + ".dat")
-            shutil.copyfile(file, filename + ".dat")
-            
+            # otherwise
+            else:
+                # get corrected filename
+                filename = getCorrectedUUID(getUUID(parts[0]))
+                # copy files
+                if exists(filename + ".dat"):
+                    print("File " + filename + ".dat" + " already exists, keeping old version.")
+                    continue
+                # debug logging
+                print("Copying file: " + file + " -> " + filename + ".dat")
+                shutil.copyfile(file, filename + ".dat")
+    if userinput == "2":
+        if exists("players.txt"):
+            print("Deleting layers.txt ...")
+            remove("players.txt")
+        for file in getFiles():
+            # split name and extension
+            parts = file.split(".")
+            # if the extension is not .dat
+            if(parts[1] != 'dat'):
+                print(file + " is not a .dat file, ignoring.")
+                continue
+            # otherwise
+            else:
+                with open('players.txt', 'a') as writer:
+                    writer.write(getName(parts[0]) + "\n")
+                    writer.close()
 
         
 # python trickery
